37ad3be0cc402e034e42590920e2ff35b6b2406d
Andres Valencia <cryos74@gmail.com>
2025-03-17 20:54:18 -0500

    Vite config

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://andresV74.github.io/portfolio",
+  base: "/portfolio/",
 })
